b8b403f98f5e7459773717ad6aa4e13455bd3248
Nicolás Mariano Rodríguez <84926262+nm-rodriguez@users.noreply.github.com>
2024-06-03 20:09:54 -0300

    Delete JavaScript - DevTalles directory

--- JavaScript - DevTalles/03-BlackJack-VITE/src/blackjack/usecases/card-value.js
+++
@@ -1,9 +0,0 @@
-/**
- * 
- * @param {string} card 
- * @returns {int} returns an integer
- */
-export const cardValue = ( card ) => {
-    const value = card.substring(0,card.length-1);
-    return (isNaN(value)) ? (( value === 'A') ? 11 : 10) : value * 1;
-}
